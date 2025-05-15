cbc0e6c4e7a7a0416c52477744cc6473a6ae1660
Yassinproweb <yassinkatungi67@gmail.com>
2025-05-15 23:58:30 +0300

    html in index

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,13 @@ module.exports = {
         cs_color: 'hsl(187, 41%, 43%)',
         cs_white: 'hsl(187, 1%, 100%)',
         cs_black: 'hsl(187, 1%, 07%)',
-        ord_plc: 'hsl(217, 71%, 57%)', //blue
-        ord_pdg: 'hsl(277, 71%, 57%)', //purple
-        ord_cld: 'hsl(217, 11%, 83%)', //gray
-        ord_rdy: 'hsl(47, 81%, 53%)', //orange
-        ord_trs: 'hsl(77, 81%, 53%)', //yellow
-        ord_dlv: 'hsl(147, 71%, 37%)', //green
-        ord_dnd: 'hsl(1, 71%, 47%)', //red
+        ord_plc: 'hsl(217, 81%, 57%)', //blue
+        ord_pdg: 'hsl(267, 81%, 53%)', //purple
+        ord_cld: 'hsl(217, 7%, 71%)', //gray
+        ord_rdy: 'hsl(37, 87%, 53%)', //orange
+        ord_trs: 'hsl(53, 87%, 53%)', //yellow
+        ord_dlv: 'hsl(147, 81%, 37%)', //green
+        ord_dnd: 'hsl(1, 81%, 47%)', //red
       }
     },
   },

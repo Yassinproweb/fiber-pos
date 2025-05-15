60e3cdde1904a9448508533ed852c7e32ab87bea
Yassinproweb <yassinkatungi67@gmail.com>
2025-05-15 23:26:09 +0300

    reading user_row

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html/v2"
 )
@@ -17,17 +19,36 @@ func main() {
 		Views: engine,
 	})
 
+	router.Static("/static", "./static")
+
 	router.Get("/", func(c *fiber.Ctx) error {
 		users := GetUsers()
 		return c.Render("index", fiber.Map{
 			"users": users,
 		})
 	})
+
+	router.Post("/users", func(c *fiber.Ctx) error {
+		user := User{
+			Name:  c.FormValue("name"),
+			Email: c.FormValue("email"),
+		}
+		return c.Render("user_row", user)
+	})
+
+	router.Delete("/users/:name", func(c *fiber.Ctx) error {
+		name := c.Params("name")
+		fmt.Println("Delete user called ", name)
+		return c.SendStatus(fiber.StatusOK)
+	})
+
+	fmt.Println("Server is running on port 5174")
+	router.Listen(":5174")
 }
 
 func GetUsers() []User {
 	return []User{
 		{Name: "Katungi Yassin", Email: "yassinkatungi67@gmail.com"},
-		{Name: "Nnassaazi Salwa", Email: "nnassaazisalwa95@gmail.com"},
+		{Name: "Nnansereko Hajarah", Email: "nnanserekohajarah95@gmail.com"},
 	}
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,16 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        posblue100: 'oklch(0.72, 0.11, 173)',
+        cs_color: 'hsl(187, 41%, 43%)',
+        cs_white: 'hsl(187, 1%, 100%)',
+        cs_black: 'hsl(187, 1%, 07%)',
+        ord_plc: 'hsl(217, 71%, 57%)', //blue
+        ord_pdg: 'hsl(277, 71%, 57%)', //purple
+        ord_cld: 'hsl(217, 11%, 83%)', //gray
+        ord_rdy: 'hsl(47, 81%, 53%)', //orange
+        ord_trs: 'hsl(77, 81%, 53%)', //yellow
+        ord_dlv: 'hsl(147, 71%, 37%)', //green
+        ord_dnd: 'hsl(1, 71%, 47%)', //red
       }
     },
   },

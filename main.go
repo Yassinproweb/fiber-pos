63747b258df8afe5b8824610d29ac1daf7f78d1b
Yassinproweb <yassinkatungi67@gmail.com>
2025-07-05 15:53:13 +0300

    table working with orders

--- main.go
+++ main.go
@@ -7,9 +7,29 @@ import (
 	"github.com/gofiber/template/html/v2"
 )
 
-type User struct {
-	Name  string
-	Email string
+type Type string
+type Status string
+
+const (
+	Takeaway Type = "Takeaway"
+	Delivery Type = "Delivery"
+	DineIn   Type = "DineIn"
+
+	Placed    Status = "Placed"
+	Pending   Status = "Pending"
+	Ready     Status = "Ready"
+	Canceled  Status = "Canceled"
+	Delivered Status = "Delivered"
+	Taken     Status = "Taken"
+	Served    Status = "Served"
+)
+
+type Order struct {
+	Name   string
+	Type   Type
+	Status Status
+	Items  string
+	Cost   string
 }
 
 func main() {
@@ -22,33 +42,37 @@ func main() {
 	router.Static("/static", "./static")
 
 	router.Get("/", func(c *fiber.Ctx) error {
-		users := GetUsers()
+		orders := FetchOrders()
 		return c.Render("index", fiber.Map{
-			"users": users,
+			"orders": orders,
 		})
 	})
 
-	router.Post("/users", func(c *fiber.Ctx) error {
-		user := User{
-			Name:  c.FormValue("name"),
-			Email: c.FormValue("email"),
+	router.Post("/orders", func(c *fiber.Ctx) error {
+		order := Order{
+			Name:   c.FormValue("name"),
+			Type:   Type(c.FormValue("type")),
+			Status: Status(c.FormValue("status")),
+			Items:  c.FormValue("items"),
+			Cost:   c.FormValue("cost"),
 		}
-		return c.Render("user_row", user)
-	})
-
-	router.Delete("/users/:name", func(c *fiber.Ctx) error {
-		name := c.Params("name")
-		fmt.Println("Delete user called ", name)
-		return c.SendStatus(fiber.StatusOK)
+		fmt.Println(order)
+		return c.Render("order", order)
 	})
 
 	fmt.Println("Server is running on port 5174")
 	router.Listen(":5174")
 }
 
-func GetUsers() []User {
-	return []User{
-		{Name: "Katungi Yassin", Email: "yassinkatungi67@gmail.com"},
-		{Name: "Nnansereko Hajarah", Email: "nnanserekohajarah95@gmail.com"},
+func FetchOrders() []Order {
+	return []Order{
+		{"#ORD0011", DineIn, Placed, "2", "7.33"},
+		{"#ORD0012", Takeaway, Taken, "1", "3.85"},
+		{"#ORD0013", DineIn, Canceled, "7", "22.00"},
+		{"#ORD0014", Delivery, Pending, "8", "102.79"},
+		{"#ORD0015", DineIn, Served, "5", "33.5"},
+		{"#ORD0016", Delivery, Delivered, "13", "123.84"},
+		{"#ORD0017", Takeaway, Ready, "2", "13.75"},
+		{"#ORD0018", DineIn, Pending, "3", "23.05"},
 	}
 }

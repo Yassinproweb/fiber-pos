9ac490d7592d3a5c8ae0bb58a5c4658eceb7446b
Yassinproweb <yassinkatungi67@gmail.com>
2025-07-06 10:57:43 +0300

    colors update

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,17 +6,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        cs_color: 'hsl(81, 21%, 35%))',
-        cs_white: 'hsl(205, 1%, 100%)',
-        cs_light: 'hsl(205, 1%, 63%)',
-        cs_black: 'hsl(205, 1%, 07%)',
+        pos_col: 'hsl(71, 31%, 45%)',
+        pos_whi: 'hsl(205, 1%, 100%)',
+        pos_lgh: 'hsl(205, 1%, 63%)',
+        pos_bla: 'hsl(205, 1%, 07%)',
 
-        ord_plc: 'hsl(217, 99%, 56%)',
-        ord_cld: 'hsl(357, 95%, 50%)',
-        ord_trs: 'hsl(81, 97%, 45%)',
-        ord_dlv: 'hsl(141, 89%, 43%)',
-        ord_pdg: 'hsl(267, 81%, 57%)',
-        ord_rdy: 'hsl(51, 93%, 45%)',
+        pos_plc: 'hsl(217, 99%, 56%)',
+        pos_cld: 'hsl(357, 95%, 50%)',
+        pos_trs: 'hsl(81, 97%, 45%)',
+        pos_dlv: 'hsl(141, 89%, 43%)',
+        pos_pdg: 'hsl(267, 81%, 57%)',
+        pos_rdy: 'hsl(51, 93%, 45%)',
       }
     },
   },
